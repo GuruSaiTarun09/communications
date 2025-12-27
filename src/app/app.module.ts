1dc68ab7616ed2d80aba58307aa8e1d47c9ec63a
GuruSaiTarun09 <gurusaigithub@gmail.com>
2025-12-27 15:24:30 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateproductComponent } from './components/createproduct/createproduct.component';
+import { AllproductsComponent } from './components/allproducts/allproducts.component';
+import { ViewproductComponent } from './components/viewproduct/viewproduct.component';
+import { CreatebuttonComponent } from './components/createbutton/createbutton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CreateproductComponent,
+    AllproductsComponent,
+    ViewproductComponent,
+    CreatebuttonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/createbutton/createbutton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createbutton',
+  templateUrl: './createbutton.component.html',
+  styleUrls: ['./createbutton.component.css']
+})
+export class CreatebuttonComponent {
+
+}
