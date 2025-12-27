d8f0fbeeb7b1fe742d1cc49829dae2cbaf27f236
GuruSaiTarun09 <gurusaigithub@gmail.com>
2025-12-27 16:46:26 +0530

    path is completed

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+    products: Product[] = [];
+  selectedProduct!: Product | null;
 
+  addProduct(product: Product) {
+    this.products.push(product);
+  }
+
+  viewProduct(product: Product) {
+    this.selectedProduct = product;
+  }
+
+  deleteProduct(id: number) {
+    this.products = this.products.filter(p => p.id !== id);
+    if (this.selectedProduct?.id === id) {
+      this.selectedProduct = null;
+    }
+  }
 }
